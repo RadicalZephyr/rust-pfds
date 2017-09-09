a4c301a333306234a5b0d66c279a5cc22e059d4e
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-09 14:58:06 -0500

    Add a super basic sequence trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
+pub trait Sequence<E> {
+    type R1: Sequence<E>;
+    type R2: Sequence<E>;
+    type R3: Sequence<E>;
+
+    fn cons(&self, el: E) -> Self::R1;
+
+    fn first(&self) -> (E, Self::R2);
+
+    fn rest(&self) -> Self::R3;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
