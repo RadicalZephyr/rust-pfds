62903deaa7ad2fe8199253240d1428740b2e12c5
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-09 16:41:03 -0500

    Make lists on usable from inside Rc's

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use std::rc::Rc;
+
 pub trait Sequence<E> {
     type R1: Sequence<E>;
     type R2: Sequence<E>;
 
-    fn cons(self, el: E) -> Self::R1;
+    fn cons(&self, el: E) -> Self::R1;
 
     fn first(&self) -> Option<&E>;
 
@@ -12,34 +14,33 @@ pub trait Sequence<E> {
 #[derive(Debug, PartialEq)]
 enum List<E> {
     Nil,
-    Cons(E, Box<List<E>>),
+    Cons(E, Rc<List<E>>),
 }
 
 impl<E> List<E> {
-    pub fn new() -> Self {
-        List::Nil
+    pub fn new() -> Rc<Self> {
+        Rc::new(List::Nil)
     }
 }
 
-impl<E> Sequence<E> for List<E> {
-    type R1 = Self;
-    type R2 = Self;
+impl<E> Sequence<E> for Rc<List<E>> {
+    type R1 = Rc<List<E>>;
+    type R2 = Rc<List<E>>;
 
-    fn cons(self, el: E) -> Self::R1 {
-        List::Cons(el, Box::new(self))
+    fn cons(&self, el: E) -> Self::R1 {
+        Rc::new(List::Cons(el, self.clone()))
     }
 
     fn first(&self) -> Option<&E> {
         use List::*;
-
-        match *self {
+        match **self {
             Nil => None,
             Cons(ref el, _) => Some(el),
         }
     }
 
     fn rest(&self) -> Self::R2 {
-        List::Nil
+        Rc::new(List::Nil)
     }
 }
 
@@ -49,14 +50,22 @@ mod tests {
 
     #[test]
     fn first_on_nil() {
-        let l: List<u8> = List::new();
+        let l: Rc<List<u8>> = List::new();
         assert_eq!(l.first(), None);
     }
 
     #[test]
     fn first_on_singleton() {
-        let l: List<u8> = List::new().cons(1);
+        let l = List::new().cons(1);
         let el = l.first();
         assert_eq!(el, Some(&1));
+    }
+
+    #[test]
+    fn does_not_move_on_cons() {
+        let l = List::new();
+        let l2 = l.cons(1);
+        assert_eq!(*l, List::Nil);
+        assert_eq!(l2.first(), Some(&1));
     }
 }
