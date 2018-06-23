4dba3b59a0fb12a38428259a6c77c8c7426ff9c9
Geoff Shannon <geoffpshannon@gmail.com>
2018-06-23 01:26:29 -0700

    Add the iterate function from clojure

--- src/set.rs
+++ src/set.rs
@@ -90,6 +90,29 @@ where E: Clone + PartialOrd
     }
 }
 
+struct Iterate<I, F> {
+    current: I,
+    f: F,
+}
+
+impl<I, F> Iterator for Iterate<I, F>
+where F: Fn(&I) -> I,
+{
+    type Item = I;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use std::mem;
+        let next = (self.f)(&self.current);
+        Some(mem::replace(&mut self.current, next))
+    }
+}
+
+fn iterate<I, F>(init: I, f: F) -> impl Iterator<Item = I>
+where F: Fn(&I) -> I,
+{
+    Iterate { current: init, f}
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -126,5 +149,17 @@ mod tests {
         assert!(t2.member(&2));
         assert!(t2.member(&3));
         assert_eq!(Rc::strong_count(&t.0), 2);
+    }
+
+    #[test]
+    fn iterate_inc() {
+        let mut res = iterate(5, |x| x + 1);
+        assert_eq!(vec![5, 6, 7, 8], res.take(4).collect::<Vec<u8>>());
+    }
+
+    #[test]
+    fn iterate_powers_of_two() {
+        let mut res = iterate(1, |x| x * 2);
+        assert_eq!(vec![1, 2, 4, 8], res.take(4).collect::<Vec<u8>>());
     }
 }
